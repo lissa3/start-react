67be498da744513410398b46093c5af26c7bde5f
Tanja Rietveldt <diafox2015@gmail.com>
2022-11-30 22:01:46 +0100

    firstcommit; animals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import AnimalShow from "./components/AnimalShow";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() {        
+  const getAnimal = ()=>{
+      let arr = ["bird","cow","horse","dog","gator","cat","horse"]
+      const idx = Math.round(Math.random()*(arr.length -1));         
+      return arr[idx]
+    }
+    
+    const [animals, setAnimal] = useState([]);     
+    const handleClick = (e)=>{               
+      setAnimal([...animals,getAnimal()])  
+           
+    } 
+    // iteration: applly call-back for each elem in arr(wrap in comp) 
+    const listAnimals =  animals.map((animal,index)=>{
+      return <AnimalShow type={animal} key={index}></AnimalShow>   
+      
+    })  
+    return(
+      <div className="app">        
+        <button onClick={handleClick}>
+          Add animal
+        </button>        
+        <div className="animal-list">{listAnimals}</div>                  
+          
+      </div>      
+    )
 }
 
 export default App;
