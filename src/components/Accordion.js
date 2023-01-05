a44db33cc1220ef6ee8e5f9b2292ac93b45dc004
tata <tspan2017@gmail.com>
2023-01-05 20:30:33 +0100

    add notes: setVal with callback to solve some poss bugs

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -8,28 +8,24 @@ const Accordion = ({items}) => {
     // use hybride arrow func + named event handler:
     // + reach scope 
     const handleClick = (idx)=>{
-       setExpandedIndex((current)=>{
-        if(current ===idx){
+        // func way to update the state
+        // cuurent == most updated val in state val
+        // tech way: if a new val depands on the one(very quickly need to update)
+        // prevent small bug: delayed update: simulate 2 clickes one-by-one
+        // current is the most recent val triggers change-re-render
+       setExpandedIndex((currentExtendedIndex)=>{
+        if(currentExtendedIndex ===idx){
             return null
         }else{
+            // return a new val (without delay)
             return idx
         }
        })
-    }
-    // const handleClick = (idx)=>{
-    //     if(expendedIndex === idx){
-    //         setExpandedIndex(null)
-    //     }else{
-    //         setExpandedIndex(idx)
-    //     }
-    // }
-    
+    }       
      
     const renderedItems = items.map((item,index)=>{
         const isExtended = index === expendedIndex;
-        const icon = <span className='text-2xl'>{isExtended?<GoTriangleDown/>:<GoTriangleLeft/>}</span> 
-        // conditional rendering
-        // const content= isExtended&&<div>{item.content}</div>        
+        const icon = <span className='text-2xl'>{isExtended?<GoTriangleDown/>:<GoTriangleLeft/>}</span>                
         return (
           <div key={item.id}>
             <div onClick ={()=>handleClick(index)} className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer">
